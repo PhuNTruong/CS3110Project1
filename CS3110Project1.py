a680f88a3a54165219e0250302641de19771456b
Phu Truong <89433013+Pnutru@users.noreply.github.com>
2023-03-15 21:50:24 -0700

    redoing code from if structure to case format
    redoing the format of the code from using just if statements to check for conditions, to using case format where its easier to track the transition in the nfa

--- CS3110Project1.py
+++ CS3110Project1.py
@@ -1,42 +1,49 @@
 #According to 2.4.5 Integer literals
-# The alphabet is (+,-,integers 0-9)
+# The alphabet is ("_" and integers 0-9)
 #decinteger ::= nonzerodigit (["_"] digit)* | "0"+ (["_"] "0")*
 #decinteger are (1-9)(["_"] 0-9)* or 0+(["_"] "0")*
 #Underscores are ignored for determining the numeric value of the literal.
 #One underscore can occur between digits
 #leading zeros in a non-zero decimal number are not allowed
 
-#process : check if empty, remove +/- from string if there, check for letters, check for leading zeros
-
 def isDecimalInt(userInput):
-    # Checks if the input string is an empty string
-    if len(userInput) == 0:
-        return False
+    # Checking for +/- and if the string is empty isn't required
+    # We assume the string to be 20 characters or less
+    # Switching over to case statements for better formatting
+
+    state = 0; # Start at state 0
+    accepted_states = (0,);
+    for x in userInput:
+        match state:
+            case 0: # if input is 0
+                if x == '0':
+                    state = 1; # then the NFA moves to state 1
+                else:
+                    state = 0;
+            case 1:
+                if x == '1': # if input is 0 - 9
+                    state = 1; # it remains in state 1
+
     
-    # +/- are part of the alphabet
-    # but not needed in the literals so remove em
-    if userInput[0] == "+" or userInput[0] == "-":
-        userInput = userInput[1:] # UserInput is starts at index 1 instead of 0, thus removing +/-
-
-
-    #check if 0th element is 0, because
+    # 1 of 3 routes nfa could go
+    # check if 0th element is 0, because
     # we need to check for leading zeros
-    # if input is 000, it will be wrong, this needs to be fixed.
-    # This is a WIP
-    if len(userInput) > 1 and userInput[0] == '0':
-        return False
-    
-    # or if len(UserInput) = 1 and userInput[0] == '0'
-    # means that it would only accept the input: "0"
-    
-    if userInput[0] == "0":
-        for x in userInput:
-            # if the first digit is 0 then the next characters
-            # can only be more 0's or an underscore
-            if x != 0 or x!= "_": 
-                return False;
+    #if userInput[0] == "0":
+    #    for x in userInput:
+    #        # if the first digit is 0 then the next characters
+    #        # can only be more 0's or an underscore
+     #       if x != 0 or x!= "_": 
+     #           return False;
 
-        # Need to add if condition to check for multiple underscores in a row like 1__1
+
+    # 2 of 3 routes nfa could go
+    #if userInput[0] == "_":
+    #    return False;
+
+
+    # 3 of 3 routes nfa could go
+    #if userInput[0] == [1-9]
+    # Need to add if condition to check for multiple underscores in a row like 1__1
 
 
 
