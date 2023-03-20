1d57e30c750c44ed5cf66c86e3dbef70b311706e
Amar Gandhi <amargandhi10@gmail.com>
2023-03-20 14:39:44 -0700

    Convert isDecimalInt to DFA with null states to properly reject bad inputs

--- CS3110Project1.py
+++ CS3110Project1.py
@@ -8,8 +8,9 @@
 
 # Constants using python sets
 BIN_DIGIT = {'0', '1'}
-OCT_DIGIT = {str(x) for x in range(1, 8)}
-DEC_DIGIT = {str(x) for x in range(1, 10)}
+OCT_DIGIT = {str(x) for x in range(0, 8)}
+NON_ZERO_DEC_DIGIT = {str(x) for x in range(1, 10)}
+DEC_DIGIT = NON_ZERO_DEC_DIGIT.union({'0'})
 HEX_DIGIT = DEC_DIGIT.union({'A', 'B', 'C', 'D', 'E', 'F', 'a', 'b', 'c', 'd', 'e', 'f'})
 
 def isDecimalInt(userInput):
@@ -17,52 +18,59 @@ def isDecimalInt(userInput):
     # We assume the string to be 20 characters or less
     # Switching over to case statements for better formatting
 
-    state = 0; # Start at state 0
-    accepted_states = (1,3,);
+    state = 0   # Start at state 0
+    accepted_states = {1, 3}   # set of accepted states
     for x in userInput:
         match state:
-            case 0: 
+            case 0:
                 # if input is 0, go to q1
                 # if input is 1-9, go to q3
-                # if input is "_" go to q4
+                # otherwise reject
                 if x == '0':
-                    state = 1;
-                elif x in [(userInput(x) for x in range(1,10))]: 
-                    state = 3;
+                    state = 1
+                elif x in NON_ZERO_DEC_DIGIT:
+                    state = 3
+                else:
+                    state = 'null'
+            case 1:
+                # stay in 1 if x is 0
+                # go to 2 if underscore
+                # otherwise reject
+                if x == '0':
+                    state = 1
                 elif x == '_':
-                    state = 4;
-            
-            case 1: 
-                # if input is 0, stay in q1
-                # if input is 1-9, go to q2
+                    state = 2
+                else:
+                    state = 'null'
+            case 2:
+                # go back to 1 if x is 0
+                # otherwise reject
                 if x == '0':
-                    state = 1;
-                elif x in [(userInput(x) for x in range(1,10))]:
-                    state = 2;
-            
-            case 2: 
-                # Once here, any input will still remain here
-                state = 2;
-                
-            case 3: 
+                    state = 1
+                else:
+                    state = 'null'
+            case 3:
                 # if input is 0-9, stays in q3
                 # If input is "_", go to q5
-                if x in [(userInput(x) for x in range(0,10))]:
-                    state = 3;
+                # reject anything else
+                if x in DEC_DIGIT:
+                    state = 3
                 elif x == '_':
-                    state = 5;
-                
-            case 4: 
-                # Once here, any input will still remain here
-                state = 4;
-            
+                    state = 5
+                else:
+                    state = 'null'
             case 5:
                 # if input is 0-9, go to q3
-                # if input is "_", go to q4
-                if x in [(userInput(x) for x in range(0,10))]:
+                # if input is "_", reject
+                if x in DEC_DIGIT:
                     state = 3;
                 elif x == '_':
-                    state = 4;
+                    state = 'null';
+                else:
+                    state = 'null'
+            case 'null':
+                # null state, do nothing
+                pass
 
     return(state in accepted_states)
 
